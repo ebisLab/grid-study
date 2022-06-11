7f7652fe780d9dbd62062d2965fae5a52e458c99
ebisLab <ebaiden@techie.com>
2022-06-11 05:47:31 -0400

    understanding basic grid pattern

---
+++ src/Layout1.js/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+export default function index() {
+  return (
+    <div className='grid-container'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+    </div>
+  )
+}
